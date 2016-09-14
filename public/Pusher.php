af2864e15be185b41a61d0dd589249264dc06f7a
Stephan Hoogland <stephan@shoogland.com>
2016-09-14 11:22:22 +0200

    added endpoints for pusher

---
+++ public/Pusher.php
@@ -0,0 +1,17 @@
+<?php
+
+class Pusher
+{
+    public function score()
+    {
+        $response = array('status' => 'success');
+
+        return $response;
+    }
+    public function karaoke()
+    {
+        $response = array('status' => 'success');
+
+        return $response;
+    }
+}
